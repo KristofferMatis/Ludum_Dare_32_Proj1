ca856b92c0593e74e5d1c0f958093b3ce73107fc
mati0029 <mati0029@algonquinlive.com>
2015-04-18 04:17:27 -0400

    stubs made for crafting menu

--- Game1/Assets/Scripts/CraftingMenu/CrafingMenu.cs
+++ Game1/Assets/Scripts/CraftingMenu/CrafingMenu.cs
@@ -1,15 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CrafingMenu : MonoBehaviour {
-
+public class CrafingMenu : Singleton<InputManager>
+{
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+    {
 	
 	}
+
+    void EnterMenu()
+    {
+        //TODO: enable Curser
+        //TODO: make curser visible
+
+        //TODO: make menu Visable
+
+        //TODO: get slot data from weapon
+        //TODO: get item list from inventory
+
+        //TODO: create the menus slot list
+        //TODO: create the menus item list
+    }
+
+    void ExitMenu()
+    {
+        //TODO: Disable Curser
+        //TODO: make curser Invisible
+
+        //TODO: make menu InVisable
+
+        //TODO: set weapon slot data
+        //TODO: set  inventory item list
+    }
 }
