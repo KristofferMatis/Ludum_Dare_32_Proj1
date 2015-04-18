14f1b0c8f2ddd61b3865c56b3511ff2e23b967c7
Diego Henriquez <henr0179@algonquinlive.com>
2015-04-18 02:32:16 -0400

    did some stuff now going to sleep :D

--- Game1/Assets/Scripts/Camera/ChaseCamera.cs
+++ Game1/Assets/Scripts/Camera/ChaseCamera.cs
@@ -3,16 +3,23 @@ using System.Collections;
 
 public class ChaseCamera : CameraBase
 {
-    
+    public Vector3 CameraDistance = new Vector3(0, 2, -5);
+    public GameObject LookAtObject;
+
+    Vector3 m_Offset;
     // Use this for initialization
     void Start()
     {
+        m_Offset = LookAtObject.transform.position + CameraDistance;
 
+        transform.position = m_Offset;
     }
 
     // Update is called once per frame
     void Update()
     {
+        m_Offset = LookAtObject.transform.position + CameraDistance;
 
+        transform.position = m_Offset;
     }
 }
