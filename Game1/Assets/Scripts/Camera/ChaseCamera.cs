c05fb96fd1feb556285b1dc0c13fcdbd92dd806d
Diego Henriquez <henr0179@algonquinlive.com>
2015-04-18 14:40:28 -0400

    Some changes to camera
    doing some changes :p

--- Game1/Assets/Scripts/Camera/ChaseCamera.cs
+++ Game1/Assets/Scripts/Camera/ChaseCamera.cs
@@ -4,9 +4,14 @@ using System.Collections;
 public class ChaseCamera : CameraBase
 {
     public Vector3 CameraDistance = new Vector3(0, 2, -5);
+	public float CameraRaiseSpeed = 1.0f;
+	public float CameraMaxHeight = 4.0f;
+	public float CameraMinHeight = -0.5f;
 	public float CameraMoveEaseSpeed = 4.0f;
 	public float CameraLookEaseSpeed = 3.0f;
 	public float CameraRotationSpeed = 4.0f;
+	public float WobbleStrength = 0.5f;
+	public float WobbleSpeed = 1.2f;
     public GameObject LookAtObject;
 
 	float m_CurrentAngle = 0.0f;
@@ -19,6 +24,11 @@ public class ChaseCamera : CameraBase
         transform.position = m_Offset;
     }
 
+	public override void CameraHandleFixedUpdate()
+	{
+	
+	}
+
 	public override void CameraHandleUpdate()
 	{
 		MoveCamera ();
@@ -30,9 +40,10 @@ public class ChaseCamera : CameraBase
 		float lookLRDir = InputManager.Instance.PlayerLookRightLeft ();
 		float lookUDDir = InputManager.Instance.PlayerLookUpDown ();
 
+
+		ChangeCameraDistanceHeight (ref CameraDistance);
 		m_Offset = LookAtObject.transform.position + CameraDistance;
 
-		Quaternion rotation =  Quaternion.Euler(0, m_CurrentAngle,0);
 
 		//testing only with mac because controller is wierd and no mouse :p
 		if(Input.GetKey(KeyCode.E))
@@ -43,6 +54,9 @@ public class ChaseCamera : CameraBase
 		{
 			m_CurrentAngle += CameraRotationSpeed * Time.deltaTime;
 		}
+
+		Quaternion rotation = Quaternion.Euler (0, m_CurrentAngle, 0);
+
 		Vector3 goal = RotateAroundPoint (m_Offset, LookAtObject.transform.position, rotation);
 
 		transform.position = Vector3.Lerp (transform.position, goal, CameraMoveEaseSpeed * Time.deltaTime);
@@ -54,8 +68,31 @@ public class ChaseCamera : CameraBase
 		Vector3 dir = LookAtObject.transform.position - transform.position;
 
 		Quaternion rotation = Quaternion.LookRotation (dir, Vector3.up);
+		Vector3 temp;
+
+		float isMovingForward = InputManager.Instance.PlayerMoveForwardBack();
+		float isMovingRight = InputManager.Instance.PlayerMoveRightLeft();
+
+		if(isMovingForward != 0 || isMovingRight != 0)
+		{
+			temp = new Vector3(0,0,Mathf.Sin (Time.time * WobbleSpeed) * WobbleStrength);
+			rotation *= Quaternion.Euler (temp);
+		}
 
 		transform.rotation = Quaternion.Lerp (transform.rotation, rotation, CameraLookEaseSpeed * Time.deltaTime);
+	}
+
+	void ChangeCameraDistanceHeight(ref Vector3 cameraDist)
+	{
+
+		if(Input.GetKey(KeyCode.R) && CameraDistance.y <= CameraMaxHeight)
+		{
+			cameraDist.y += CameraRaiseSpeed * Time.deltaTime;
+		}
+		if(Input.GetKey(KeyCode.F) && CameraDistance.y >= CameraMinHeight)
+		{
+			cameraDist.y -= CameraRaiseSpeed * Time.deltaTime;
+		}
 	}
 
 	Vector3 RotateAroundPoint(Vector3 aPos, Vector3 aPivot, Quaternion angle)
