ad519efbf97e52a1814bc15e5569789a673b5656
Diego Henriquez <henr0179@algonquinlive.com>
2015-04-18 01:06:30 -0400

    Commited input manager and camera stuff :D

---
+++ Game1/Assets/Scripts/Camera/CameraBase.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class CameraBase : MonoBehaviour
+{
+    public virtual void Activate()
+    {
+
+    }
+
+    public virtual void Deactivate()
+    {
+
+    }
+
+    public virtual void CameraHandleUpdate()
+    {
+
+    }
+}
---
+++ Game1/Assets/Scripts/Camera/CameraManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour
+{
+    public CameraBase CameraMovement;
+
+
+    CameraBase m_CurrentCameraAction;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
