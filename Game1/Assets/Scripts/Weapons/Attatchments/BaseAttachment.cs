a4046e2835706ba6fd4a087f02f44e20a0ee84e1
Meetic666 <bellay.quentin@gmail.com>
2015-04-18 02:43:43 -0400

    Updated WeaponStats and BaseAttachment
    Everything is attachments.
    
    Will code the pick up behaviour and fighting behaviour tomorrow morning ;-)

--- Game1/Assets/Scripts/Weapons/Attatchments/BaseAttachment.cs
+++ Game1/Assets/Scripts/Weapons/Attatchments/BaseAttachment.cs
@@ -1,15 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class BaseAttachment : MonoBehaviour {
+public class BaseAttachment : MonoBehaviour 
+{
+	public WeaponStats m_Stats;
 
-	// Use this for initialization
-	void Start () {
-	
+	public WeaponStats GetStats()
+	{
+		WeaponStats stats = new WeaponStats (m_Stats);
+
+		foreach(Transform attachment in m_Stats.m_MountPoints)
+		{
+			if(attachment.GetChild (0) != null)
+			{
+				WeaponStats attachmentStats = attachment.GetChild (0).GetComponent<BaseAttachment>().GetStats ();
+
+				stats.m_Damage += attachmentStats.m_Damage;
+				stats.m_Knockback += attachmentStats.m_Knockback;
+				stats.m_PlayerSpeed += attachmentStats.m_PlayerSpeed;
+				stats.m_StartUpTime += attachmentStats.m_StartUpTime;
+				stats.m_RecoveryTime += attachmentStats.m_RecoveryTime;
+			}
+		}
+
+		return stats;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	public void AddAttachment(GameObject newAttachment, int mountPointIndex)
+	{
+		if(mountPointIndex < m_Stats.m_MountPoints.Count)
+		{
+			newAttachment.transform.parent = m_Stats.m_MountPoints[mountPointIndex];
+		}
+	}
+
+	public GameObject RemoveAttachment(int mountPointIndex)
+	{
+		if(mountPointIndex < m_Stats.m_MountPoints.Count)
+		{
+			GameObject attachment = m_Stats.m_MountPoints [mountPointIndex].GetChild (0);
+			attachment.transform.parent = null;
+
+			return attachment;
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	public GameObject GetAttachment(int mountPointIndex)
+	{
+		if(mountPointIndex < m_Stats.m_MountPoints.Count)
+		{
+			GameObject attachment = m_Stats.m_MountPoints [mountPointIndex].GetChild (0);			
+			return attachment;
+		}
+		else
+		{
+			return null;
+		}
 	}
 }
--- Game1/Assets/Scripts/Weapons/WeaponStats.cs
+++ Game1/Assets/Scripts/Weapons/WeaponStats.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class WeaponStats : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
+public class WeaponStats 
+{
+	public int m_Damage;
+	public int m_Knockback;
+	public int m_PlayerSpeed;
+	public float m_StartUpTime;
+	public float m_RecoveryTime;
+	public List<Transform> m_MountPoints;
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public WeaponStats(WeaponStats stats)
+	{
+		m_Damage = stats.m_Damage;
+		m_Knockback = stats.m_Knockback;
+		m_PlayerSpeed = stats.m_PlayerSpeed;
+		m_StartUpTime = stats.m_StartUpTime;
+		m_RecoveryTime = stats.m_RecoveryTime;
+		m_MountPoints = stats.m_MountPoints;
 	}
 }
