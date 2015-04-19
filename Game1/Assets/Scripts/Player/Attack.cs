02a97f9ef995ed93035d0c2500dc4600020d70b4
Meetic666 <bellay.quentin@gmail.com>
2015-04-18 21:40:50 -0400

    Added menu/hand lerp

--- Game1/Assets/Scripts/Player/Attack.cs
+++ Game1/Assets/Scripts/Player/Attack.cs
@@ -3,12 +3,18 @@ using System.Collections;
 
 public class Attack : MonoBehaviour 
 {
-	public Transform m_HolsterPivotPoint;
+	public Transform m_MenuPivotPoint;
 	public Transform m_HandPivotPoint;
 
 	public BaseBaseWeapon m_WeaponEquipped;
 
+	float m_StartUpTimer;
+	float m_RecoveryTimer;
+
 	bool m_WeaponDrawn;
+
+	public float m_LerpSpeed = 1.0f;
+	public float m_RotationSpeed = 5.0f;
 
 	void Start()
 	{
@@ -19,11 +25,15 @@ public class Attack : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-	void Update () 
-	{
-		if(InputManager.Instance.PlayerAttack1()) // NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
-		{
+	public void Update () 
+	{		
+		m_WeaponEquipped.transform.localPosition = Vector3.Lerp (m_WeaponEquipped.transform.localPosition, Vector3.zero, m_LerpSpeed * Time.deltaTime);
+		m_WeaponEquipped.transform.localRotation = Quaternion.Lerp(m_WeaponEquipped.transform.localRotation, Quaternion.identity, m_LerpSpeed * Time.deltaTime);
+		m_WeaponEquipped.transform.localScale = Vector3.Lerp (m_WeaponEquipped.transform.localScale, Vector3.one, m_LerpSpeed * Time.deltaTime);
 
+		if(!m_WeaponDrawn && m_WeaponEquipped.transform.localPosition.magnitude < 0.1f)
+		{
+			m_WeaponEquipped.transform.RotateAround(m_WeaponEquipped.transform.position, Vector3.up, m_RotationSpeed * Time.deltaTime);
 		}
 	}
 
@@ -34,7 +44,7 @@ public class Attack : MonoBehaviour
 			weapon.SetTotalStatsAfterCrafting ();
 			m_WeaponEquipped = weapon;
 
-			DrawWeapon ();
+			DrawWeapon (true);
 		}
 		else
 		{
@@ -47,9 +57,9 @@ public class Attack : MonoBehaviour
 		}
 	}
 
-	void DrawWeapon()
+	public void DrawWeapon(bool isDrawn)
 	{
-		m_WeaponDrawn = !m_WeaponDrawn;
+		m_WeaponDrawn = isDrawn;
 
 		if(m_WeaponDrawn)
 		{			
@@ -57,11 +67,7 @@ public class Attack : MonoBehaviour
 		}
 		else
 		{			
-			m_WeaponEquipped.transform.parent = m_HolsterPivotPoint;
+			m_WeaponEquipped.transform.parent = m_MenuPivotPoint;
 		}
-
-		m_WeaponEquipped.transform.localPosition = Vector3.zero;
-		m_WeaponEquipped.transform.localRotation = Quaternion.identity;
-		m_WeaponEquipped.transform.localScale = Vector3.one;
 	}
 }
